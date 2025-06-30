b224d0e8f932736ae3dc0f788e2146ce117d0c37
Akshay Mestry <xa@mes3.dev>
2025-06-30 15:17:51 -0500

    build: remove unused paths workflow
    Signed-off-by: Akshay Mestry <xa@mes3.dev>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,5 +106,6 @@ ogp_site_url: str = homepage
 ogp_social_cards: dict[str, str] = {"site_url": homepage}
 ogp_enable_meta_description: bool = True
 
+
 def setup(app):
     app.add_css_file("styles.css") 
